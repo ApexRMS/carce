3118a999f0594954ca52900f36174f79ec7092a8
alex <alexe_webmail@yahoo.com>
2018-08-15 15:36:59 -0400

    Initial port of Caribou Cumulative Effects module to new platform

---
+++ Assembly.vb
@@ -0,0 +1,8 @@
+﻿'*****************************************************************************************
+' Caribou Cumulative Effects: A .NET library for simulating caribou populations
+'
+' Copyright © 2009-2015 ApexRMS.
+'
+'*****************************************************************************************
+
+<Assembly: CLSCompliant(True)> 
