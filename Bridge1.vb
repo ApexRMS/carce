1baab81284501e92f7b61a94e8d1575326209093
alex <alexe_webmail@yahoo.com>
2019-01-17 18:51:05 -0800

    Refactoring of type names for Modules to Packages conversion.

--- Bridge1.vb
+++ Bridge1.vb
@@ -1,5 +1,5 @@
 ﻿'******************************************************************************************************
-' CCE: A SyncroSim Module for simulating cumulative impacts of multiple factors on caribou populations.
+' CCE: A SyncroSim Package for simulating cumulative impacts of multiple factors on caribou populations.
 '
 ' Copyright © 2007-2018 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '******************************************************************************************************
