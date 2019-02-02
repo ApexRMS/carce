41105d9c043dc7f1613f3cff7cfe89c0bdefbb60
alex <alexe_webmail@yahoo.com>
2019-02-01 16:53:50 -0800

    Update file header blocks to match readme.md.

--- src/Assembly.vb
+++ src/Assembly.vb
@@ -1,7 +1,7 @@
-﻿'******************************************************************************************************
-' CCE: A SyncroSim Package for simulating cumulative impacts of multiple factors on caribou populations.
+﻿'******************************************************************************************************************
+' carce: SyncroSim Base Package for simulating the cumulative effects of development and climate change on caribou.
 '
 ' Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
-'******************************************************************************************************
+'******************************************************************************************************************
 
 <Assembly: CLSCompliant(True)> 
--- src/Bridge1.vb
+++ src/Bridge1.vb
@@ -1,8 +1,8 @@
-﻿'******************************************************************************************************
-' CCE: A SyncroSim Package for simulating cumulative impacts of multiple factors on caribou populations.
+﻿'******************************************************************************************************************
+' carce: SyncroSim Base Package for simulating the cumulative effects of development and climate change on caribou.
 '
 ' Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
-'******************************************************************************************************
+'******************************************************************************************************************
 
 Imports SyncroSim.Core
 Imports System.Globalization
